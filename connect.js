e4ef6f811b45c4a23de84965065f954a0d40c761
RizzyFuzz <support@rizzy.eu.org>
2024-06-02 20:20:37 +0800

    Update connect.js

--- connect.js
+++ connect.js
@@ -115,7 +115,7 @@ app.get("/api/v1/models", (req, res) => {
 app.get("/api/v1/generateImage", async (req, res) => {
   console.log("Received request for /api/v1/generateImage");
 
-  const { prompt, model, typeModel, stylePreset } = req.query;
+  const { prompt, model, typeModel, stylePreset, height, weight, sampler } = req.query;
 
   if (!prompt || !model || !typeModel) {
     return res.status(400).json({
@@ -155,7 +155,7 @@ app.get("/api/v1/generateImage", async (req, res) => {
   }
 
   try {
-    const result = typeModelLowerCase === "sdxl" ? await generateImageSDXL({ prompt, model, style_preset: stylePreset || null }) : await generateImage({ prompt, model, style_preset: stylePreset || null, "height": 1024, "width": 1024, "sampler": "DPM++ 2M Karras", });
+    const result = typeModelLowerCase === "sdxl" ? await generateImageSDXL({ prompt, model, style_preset: stylePreset || null }) : await generateImage({ prompt, model, style_preset: stylePreset || null, height: height || 1024, width: width || 1024, sampler: "DPM++ 2M Karras", });
     const { status, imageUrl } = await wait(result);
 
     if (status === "failed") {
