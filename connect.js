a4204cf2da9777054def6f7aac5d2898c75888d2
RizzyFuzz <support@rizzy.eu.org>
2024-06-03 14:30:32 +0800

    Update connect.js

--- connect.js
+++ connect.js
@@ -101,7 +101,7 @@ app.get("/api/v1/models", (req, res) => {
 app.get("/api/v1/generateImage", async (req, res) => {
     console.log(chalk.blue("Received request for /api/v1/generateImage"));
 
-    const { prompt, model, typeModel, stylePreset, height, width, negativePrompt } = req.query;
+    const { prompt, model, typeModel, stylePreset = "", height, width, negativePrompt = "" } = req.query;
 
     console.log(chalk.blue("Incoming parameters:"), {
         prompt,
